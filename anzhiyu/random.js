c12b981157d3f86ab32012d6efaf110c3c57fa3c
FSP2020 <FSP2020@qq.com>
2026-03-19 22:41:37 +0800

    Site updated: 2026-03-19 22:41:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/19/hello-world/"];function toRandomPost(){
+var posts=["2021/04/10/English Word/","2021/04/11/Phonics/","2021/09/11/FSTOOL/","2021/03/14/MC-LJXHaK/","2021/03/21/MC-Command/","2021/09/09/9-9/","2021/04/10/chemistry/","2021/04/10/English-grammar/","2023/01/14/Q/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
